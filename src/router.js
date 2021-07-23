2eb9bdc86c7ec42c24af0f32b7bcc3d705f1677c
Fern knee <kevinjbritten@gmail.com>
2021-07-23 16:22:11 -0400

    added 404 redirect

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Album from "./views/Album.vue";
 import Home from "./views/Home.vue";
+import NotFound from "./views/NotFound.vue";
 
 Vue.use(Router);
 
@@ -18,6 +19,10 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home,
+    },
+    {
+      path: "/*",
+      component: NotFound,
     },
   ],
 });
